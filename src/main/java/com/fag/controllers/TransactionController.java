3982da9daeffe45be6bd1affec4b72352096f8db
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2023-11-12 02:53:07 -0300

    Implementação de endpoint para criar uma transação entre dois usuários

--- src/main/java/com/fag/controllers/TransactionController.java
+++ src/main/java/com/fag/controllers/TransactionController.java
@@ -1,7 +1,13 @@
 package com.fag.controllers;
 
+import com.fag.domain.dtos.TransactionDTO;
+import com.fag.domain.dtos.TransactionRequestDTO;
 import com.fag.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +17,12 @@ public class TransactionController {
 
     @Autowired
     private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<TransactionDTO> createTransaction(@RequestBody TransactionRequestDTO request) throws Exception {
+        TransactionDTO newTransaction = this.transactionService.createTransaction(request);
+
+        return new ResponseEntity<>(newTransaction, HttpStatus.CREATED);
+    }
 
 }
--- src/main/java/com/fag/services/TransactionService.java
+++ src/main/java/com/fag/services/TransactionService.java
@@ -1,12 +1,16 @@
 package com.fag.services;
 
 import com.fag.domain.dtos.TransactionDTO;
+import com.fag.domain.dtos.TransactionRequestDTO;
 import com.fag.domain.dtos.UserDTO;
 import com.fag.domain.repositories.ITransactionRepository;
+import com.fag.domain.usecases.CreateTransaction;
 import com.fag.infra.jakarta.repository.JakartaTransactionRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -17,6 +21,23 @@ public class TransactionService implements ITransactionRepository {
 
     @Autowired
     private JakartaTransactionRepository repository;
+
+    @Transactional
+    public TransactionDTO createTransaction(TransactionRequestDTO request) throws Exception {
+        UserDTO sender = this.userService.findUserById(request.senderId());
+        UserDTO receiver = this.userService.findUserById(request.receiverId());
+
+        TransactionDTO dto = new TransactionDTO(
+                null,
+                request.value(),
+                sender,
+                receiver,
+                LocalDateTime.now()
+        );
+
+        CreateTransaction createTransaction = new CreateTransaction(repository);
+        return createTransaction.execute(dto);
+    }
 
     @Override
     public List<TransactionDTO> listAllTransactions() {

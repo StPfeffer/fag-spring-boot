ec616a706a1af6504772f8eb64c752ba776a4abc
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2023-11-12 03:23:13 -0300

    Criação e implementação do TransactionMocky
    Serviço externo para verificar se a transação foi aprovada ou recusada

---
+++ src/main/java/com/fag/infra/mocky/repository/TransactionMocky.java
@@ -0,0 +1,35 @@
+package com.fag.infra.mocky.repository;
+
+import com.fag.domain.repositories.ITransaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Service
+public class TransactionMocky implements ITransaction {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Override
+    public boolean authorizeTransaction() {
+        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
+
+        if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
+            if (authorizationResponse.getBody() == null) {
+                return false;
+            }
+
+            String message = (String) authorizationResponse.getBody().get("message");
+
+            return "Autorizado".equalsIgnoreCase(message);
+        }
+
+        return false;
+    }
+
+}

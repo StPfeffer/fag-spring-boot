bca4757bb55dd31e1b0a49ad9b4e012c6b97524d
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2023-11-12 03:06:59 -0300

    Criação do TransactionException
    Auxiliar na exibição dos erros referentes as transações

---
+++ src/main/java/com/fag/domain/exceptions/TransactionException.java
@@ -0,0 +1,30 @@
+package com.fag.domain.exceptions;
+
+public class TransactionException extends RuntimeException {
+    private String message;
+    private Integer statusCode;
+
+    public TransactionException(String message, Integer statusCode) {
+        super(message);
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+
+}

ae4c2a221f2952b1525add86a1ad57a94585efb8
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2023-11-12 02:30:21 -0300

    Criação da entidade JakartaTransaction

---
+++ src/main/java/com/fag/infra/jakarta/model/JakartaTransaction.java
@@ -0,0 +1,76 @@
+package com.fag.infra.jakarta.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+public class JakartaTransaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    private JakartaUser sender;
+
+    @ManyToOne
+    private JakartaUser receiver;
+
+    private LocalDateTime timestamp;
+
+    public JakartaTransaction() {
+    }
+
+    public JakartaTransaction(BigDecimal amount, JakartaUser sender, JakartaUser receiver, LocalDateTime timestamp) {
+        this.amount = amount;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.timestamp = timestamp;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public JakartaUser getSender() {
+        return sender;
+    }
+
+    public void setSender(JakartaUser sender) {
+        this.sender = sender;
+    }
+
+    public JakartaUser getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(JakartaUser receiver) {
+        this.receiver = receiver;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+}

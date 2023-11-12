390bed9ef36bcddf5bd7d1937e89a0769104f7a1
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2023-11-12 02:21:21 -0300

    Alterado TransactionDTO de record para class

--- src/main/java/com/fag/domain/dtos/TransactionDTO.java
+++ src/main/java/com/fag/domain/dtos/TransactionDTO.java
@@ -3,6 +3,40 @@ package com.fag.domain.dtos;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record TransactionDTO(Long id, BigDecimal amount, UserDTO sender, UserDTO receiver, LocalDateTime timestamp) {
+public final class TransactionDTO {
+    private final Long id;
+    private final BigDecimal amount;
+    private final UserDTO sender;
+    private final UserDTO receiver;
+    private final LocalDateTime timestamp;
+
+    public TransactionDTO(Long id, BigDecimal amount, UserDTO sender, UserDTO receiver, LocalDateTime timestamp) {
+        this.id = id;
+        this.amount = amount;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.timestamp = timestamp;
+    }
+
+    public Long id() {
+        return id;
+    }
+
+    public BigDecimal amount() {
+        return amount;
+    }
+
+    public UserDTO sender() {
+        return sender;
+    }
+
+    public UserDTO receiver() {
+        return receiver;
+    }
+
+    public LocalDateTime timestamp() {
+        return timestamp;
+    }
+
 }
 

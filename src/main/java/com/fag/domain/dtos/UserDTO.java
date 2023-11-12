f26b3585b2c856925c2f5905a3623bf48c4b0119
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2023-11-12 02:18:27 -0300

    Criação do UserDTO

---
+++ src/main/java/com/fag/domain/dtos/UserDTO.java
@@ -0,0 +1,90 @@
+package com.fag.domain.dtos;
+
+import java.math.BigDecimal;
+
+public final class UserDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String document;
+    private String email;
+    private String password;
+    private BigDecimal balance;
+    private String type;
+
+    public UserDTO(Long id, String firstName, String lastName, String document, String email, String password, BigDecimal balance, String type) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.document = document;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}

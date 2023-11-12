99dbaf70c030cd22e64aada42780dfe664ab06e4
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2023-11-12 03:14:17 -0300

    Implementação de endpoint para listar todas as transações por remetente

--- src/main/java/com/fag/controllers/TransactionController.java
+++ src/main/java/com/fag/controllers/TransactionController.java
@@ -3,6 +3,7 @@ package com.fag.controllers;
 import com.fag.domain.dtos.TransactionDTO;
 import com.fag.domain.dtos.TransactionRequestDTO;
 import com.fag.services.TransactionService;
+import com.fag.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,9 @@ public class TransactionController {
     @Autowired
     private TransactionService transactionService;
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping
     public ResponseEntity<TransactionDTO> createTransaction(@RequestBody TransactionRequestDTO request) {
         TransactionDTO newTransaction = this.transactionService.createTransaction(request);
@@ -30,8 +34,13 @@ public class TransactionController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<TransactionDTO> getTransactionById(@PathVariable Long id) {
+    public ResponseEntity<TransactionDTO> getTransaction(@PathVariable Long id) {
         return ResponseEntity.ok(this.transactionService.findTransactionById(id));
+    }
+
+    @GetMapping("/sender/{senderId}")
+    public ResponseEntity<List<TransactionDTO>> listTransactionsBySender(@PathVariable Long senderId) {
+        return ResponseEntity.ok(this.transactionService.listAllTransactionsBySender(this.userService.findUserById(senderId)));
     }
 
 }
--- src/main/java/com/fag/services/TransactionService.java
+++ src/main/java/com/fag/services/TransactionService.java
@@ -56,7 +56,7 @@ public class TransactionService implements ITransactionRepository {
 
     @Override
     public List<TransactionDTO> listAllTransactionsBySender(UserDTO sender) {
-        return null;
+        return this.repository.listAllTransactionsBySender(sender);
     }
 
     @Override

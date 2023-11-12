6059326f3927e4256dcd3a1e3c68b6c665a6cab3
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2023-11-12 02:43:40 -0300

    Implementação do método createUser

--- src/main/java/com/fag/services/UserService.java
+++ src/main/java/com/fag/services/UserService.java
@@ -2,8 +2,10 @@ package com.fag.services;
 
 import com.fag.domain.dtos.UserDTO;
 import com.fag.domain.repositories.IUserRepository;
+import com.fag.domain.usecases.CreateUser;
 import com.fag.infra.jakarta.repository.JakartaTransactionRepository;
 import com.fag.infra.jakarta.repository.JakartaUserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,12 @@ public class UserService implements IUserRepository {
 
     @Autowired
     private JakartaTransactionRepository transactionRepository;
+
+    @Transactional
+    public UserDTO createUser(UserDTO dto) {
+        CreateUser createUser = new CreateUser(repository);
+        return createUser.execute(dto);
+    }
 
     @Override
     public List<UserDTO> listAllUsers() {
